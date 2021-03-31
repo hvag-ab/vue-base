ceee137ddab16fa713d4cf65349cad5b97b3f3a8
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-03-31 15:57:56 +0800

    Update test.js

--- src/api/test.js
+++ src/api/test.js
@@ -1,9 +1,43 @@
 import request from '@/utils/http'
 
-export const test_get = (params) => {
+export const testGet = (params) => {
     return request({
-        url: 'test/te',
+        url: 'yourpath',
         method: 'get',
         params
+    })
+}
+
+export const testPost = (data) => {
+    return request({
+        url: 'yourpath',
+        method: 'post',
+        data
+    })
+}
+
+export const testUpdate = (data) => {
+    return request({
+        url: 'yourpath',
+        method: 'patch',
+        data
+    })
+}
+
+export const testDelete = (data) => {
+    return request({
+        url: 'yourpath',
+        method: 'delete',
+        data
+    })
+}
+
+# 上传文件请求
+export const upload = (data) => {
+    return request({
+        url: 'yourpath',
+        method: 'post',
+        headers: { 'content-type': 'multipart/form-data' },
+        data
     })
 }
