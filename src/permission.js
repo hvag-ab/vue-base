9817d17ef9abf14886fad892e50db1f105e0a4a9
hvag-ab <54468623+hvag-ab@users.noreply.github.com>
2021-04-30 17:21:14 +0800

    Update permission.js

--- src/permission.js
+++ src/permission.js
@@ -5,12 +5,14 @@ import NProgress from 'nprogress' // progress bar
 import 'nprogress/nprogress.css' // progress bar style
 import { getToken } from '@/utils/auth' // get token from cookie
 import getPageTitle from '@/utils/get-page-title'
+import { clearPending } from '@/utils/loading'
 
 NProgress.configure({ showSpinner: false }) // NProgress Configuration
 
 const whiteList = ['/login','/test'] // no redirect whitelist
 
 router.beforeEach(async(to, from, next) => {
+  clearPending() //跳转路由后取消跳转前的请求并清空map
   // start progress bar
   NProgress.start()
 
